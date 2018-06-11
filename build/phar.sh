a13f7b9071daea38303d92de7a4ed5eee4fd1bd6
gong023 <gon.gong.gone@gmail.com>
2018-06-11 13:55:12 -0700

    update build

--- build/phar.sh
+++ build/phar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 composer install --no-dev
-~/.composer/vendor/bin/box build -vv
+`dirname "$0"`/box build -vv
 mv ./download/phpda.phar ./download/phpda
 mv ./download/phpda.phar.pubkey ./download/phpda.pubkey
 sha1sum ./download/phpda > ./download/phpda.version
